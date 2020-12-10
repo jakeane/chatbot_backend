f1d64f76a949c8cecbeaefa8cd994a89b1d7e839
Jack Keane <john.f.keane.22@dartmouth.edu>
2020-12-09 17:03:59 -0700

    deployed to aws lambda

--- src/nrclex.js
+++ src/nrclex.js
@@ -1,13 +1,17 @@
 import { Router } from 'express';
 
+const data = require('../data/NRCLex.json');
+
 const router = Router();
 
-const data = {};
-
 const elementSum = (total, curr) => {
-  return total.map((num, i) => {
-    return num + curr[i];
-  });
+  if (curr) {
+    return total.map((num, i) => {
+      return num + curr[i];
+    });
+  } else {
+    return total;
+  }
 };
 
 router.get('/', (req, res) => {
@@ -20,15 +24,15 @@ router.get('/sentiment', (req, res) => {
   const result = message
     .split(' ')
     .map((word) => {
-      const processed = word
-        .toLowerCase()
-        .match(/[a-z’]+/g)
-        .join('');
+      if (word) {
+        const processed = word
+          .toLowerCase()
+          .match(/[a-z’]+/g)
+          .join('');
 
-      if (data[processed]) {
         return data[processed];
       } else {
-        return [0, 0, 0, 0, 0, 0, 0, 0];
+        return null;
       }
     })
     .reduce(elementSum, [0, 0, 0, 0, 0, 0, 0, 0]);
@@ -37,40 +41,3 @@ router.get('/sentiment', (req, res) => {
 });
 
 export default router;
-
-/* eslint-disable comma-dangle */
-const fs = require('fs');
-
-let currWord = '';
-let wordData;
-
-const lineReader = require('readline').createInterface({
-  input: fs.createReadStream('data/NRCLex.txt'),
-});
-
-// Emotions: [anger, anticipation, disgust, fear, joy, sadness, surprise, trust]
-
-lineReader
-  .on('line', (line) => {
-    const currLine = line.split('\t');
-
-    if (currWord !== currLine[0]) {
-      if (wordData) {
-        data[currWord] = wordData;
-      }
-
-      // eslint-disable-next-line prefer-destructuring
-      currWord = currLine[0];
-
-      wordData = [];
-
-      wordData.push(parseInt(currLine[2], 10));
-    } else if (!['positive', 'negative'].includes(currLine[1])) {
-      wordData.push(parseInt(currLine[2], 10));
-    }
-  })
-  .on('close', () => {
-    data[currWord] = wordData;
-    // console.log(data);
-    console.log('processed');
-  });
